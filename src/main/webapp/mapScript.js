090f45d6e80c84d9583ef9c82492665534337ffa
ahlschlager <ahlschlager@wisc.edu>
2020-06-26 18:01:01 +0000

    PR 6 changes

--- src/main/webapp/mapScript.js
+++ src/main/webapp/mapScript.js
@@ -1,16 +1,19 @@
-
+/**
+ * Initialize the map to show Mountain View and events in that area
+ */
 function initMap() {
 
+  // Coordinates for the default map center of Mountain View, CA
   const mapViewDefault = {lat: 37.3868, lng: -122.085}; 
 
   const map = new google.maps.Map(document.getElementById('map'), {
     zoom: 8,
-    center: mapViewDefault
+    center: mapViewDefault,
   });
   
   const geocoder = new google.maps.Geocoder();
 
-  document.getElementById('submit').addEventListener('click', function() {
+  document.getElementById('submit').addEventListener('click', () => {
     geocodeAddress(geocoder, map);
   });
 
@@ -21,8 +24,8 @@ function initMap() {
  * Finds the location on the map for the given address
  */
 function geocodeAddress(geocoder, resultsMap) {
-  let address = document.getElementById('address').value;
-  geocoder.geocode({'address': address}, function(results, status) {
+  const address = document.getElementById('address').value;
+  geocoder.geocode({'address': address}, (results, status) => {
     if (status === 'OK') {
       resultsMap.setCenter(results[0].geometry.location);
     } else {
