90352bb4ded15c220d8de9a1d69b3cc8a0f6d34a
Valeria Lopez <val.lopez1011@gmail.com>
2020-06-30 20:26:39 +0000

    Adds constant strings, imports static method

--- src/main/java/com/google/sps/servlets/ChatServlet.java
+++ src/main/java/com/google/sps/servlets/ChatServlet.java
@@ -8,6 +8,7 @@ import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.SortDirection;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableList.Builder;
+import static com.google.common.collect.ImmutableList.toImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Streams;
 import com.google.template.soy.SoyFileSet;
@@ -21,18 +22,21 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/chat")
 public class ChatServlet extends HttpServlet{
+    private static final String MESSAGE_TEXT_PROPERTY = "message-text";
+    private static final String TIMESTAMP_PROPERTY = "timestamp";
+    private static final String MESSAGE_KIND = "Message";
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 
     // Calls query on all entities of type Message
-    Query messageQuery = new Query("Message").addSort("timestamp", SortDirection.ASCENDING);
+    Query messageQuery = new Query(MESSAGE_KIND).addSort(TIMESTAMP_PROPERTY, SortDirection.ASCENDING);
     PreparedQuery preparedMessageQuery = datastore.prepare(messageQuery);
 
     // Creates list of the messages text
     ImmutableList<String> messagesList = Streams.stream(preparedMessageQuery.asIterable()).map(message -> 
-        (String) message.getProperty("message-text")).collect(ImmutableList.toImmutableList());
+        (String) message.getProperty(MESSAGE_TEXT_PROPERTY)).collect(toImmutableList());
 
     // Data will be passed in as a list of messages in a map (needed for template)
     ImmutableMap<String, ImmutableList<String>> data = ImmutableMap.of("messages", messagesList);
@@ -51,13 +55,13 @@ public class ChatServlet extends HttpServlet{
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    final String messageText = (String) request.getParameter("message-text");
+    final String messageText = (String) request.getParameter(MESSAGE_TEXT_PROPERTY);
     final long timestamp = System.currentTimeMillis();
 
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-    Entity messageEntity = new Entity("Message");
-    messageEntity.setProperty("message-text", messageText);
-    messageEntity.setProperty("timestamp", timestamp);
+    Entity messageEntity = new Entity(MESSAGE_KIND);
+    messageEntity.setProperty(MESSAGE_TEXT_PROPERTY, messageText);
+    messageEntity.setProperty(TIMESTAMP_PROPERTY, timestamp);
     datastore.put(messageEntity);
 
     response.sendRedirect("/chat");
