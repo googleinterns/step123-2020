c1b9ff146b07b51d22796258ecda506b11c981b1
Dylan Castillo <dylanjcast@gmail.com>
2020-06-30 22:21:02 +0000

    Use closure library and closure templates for events

--- src/main/webapp/events.js
+++ src/main/webapp/events.js
@@ -1,24 +1,31 @@
+goog.require('goog.dom');
+goog.require('goog.dom.TagName');
+goog.require('templates.eventsoy');
+
 function initEvents() {
   displayCalendar();
 }
 
 function displayCalendar() {
-  document.getElementById('events-calendar').innerHTML = '<iframe class="calendar-iframe" src="' 
-      + getCalendarURL() + '"></iframe>';
+  //Use Google Calendar's calendar display
+  const calendarFrame = goog.dom.createDom(goog.dom.TagName.IFRAME, {
+      'className': 'calendar-iframe', //check if className
+      'src': getCalendarURL(),
+  });
+
+  goog.dom.appendChild(goog.dom.getElementById('events-calendar'), calendarFrame);
 }
 
 function getCalendarURL() {
-  let URL = "https://calendar.google.com/calendar/embed?src=";
-
-  //Use to obtain the group id from the current page's URL
+  let calendarID = 0;
+  // Use to obtain the group id from the current page's URL
   const params = new URLSearchParams(location.search);
-
-  fetch('/calendar?groupid=' + params.get('groupid')).then(response => response.text()).then((calendarID) => {
-    URL += calendarID;
+  fetch('/calendar?groupid=' + params.get('groupid')).then(response => response.text()).then((requestedID) => {
+    calendarID = requestedID;
   });
 
-  // Check the client's timezone and add it to the calendar URL.
-  URL += "&ctz=" + Intl.DateTimeFormat().resolvedOptions().timeZone;
-
-  return URL;
+  return templates.eventsoy.obtainURL({
+      'calendarID': calendarID, 
+      'timezone': Intl.DateTimeFormat().resolvedOptions().timeZone
+  }).toString;
 }
