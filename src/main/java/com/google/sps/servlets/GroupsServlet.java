726948c6b0226d082dc9ce088db0a17f441abd90
Valeria Lopez <val.lopez1011@gmail.com>
2020-07-01 20:54:23 +0000

    Deletes HTML file, adds Javadoc

--- src/main/java/com/google/sps/servlets/GroupsServlet.java
+++ src/main/java/com/google/sps/servlets/GroupsServlet.java
@@ -14,24 +14,28 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/groups")
 public class GroupsServlet extends HttpServlet {
     
+    /**
+     * Renders a HTML template with cards representing the various
+     * groups that have already been created.
+     */
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
         Map<String, String> data = new HashMap<>();
 
         // Hard coded data for now but will use data from datastore
-        // groups are added to datastore
+        // as groups are added to datastore
         data.put("groupImage", "https://images.unsplash.com/photo-1591622414912-34f2a8f8172e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80");
         data.put("groupName", "Black Lives Matter");
         data.put("groupDescription", "Group description would go here...");
 
         SoyFileSet sfs = SoyFileSet
             .builder()
-            .add(new File("../../src/main/java/templates/pages.soy"))
+            .add(new File("../../src/main/java/templates/groups.soy"))
             .build();
         SoyTofu tofu = sfs.compileToTofu();
 
-        String out = tofu.newRenderer("templates.pages.groupsPage").setData(data).render();
+        String out = tofu.newRenderer("templates.groups.groupsPage").setData(data).render();
         response.getWriter().println(out);
     }
 }
