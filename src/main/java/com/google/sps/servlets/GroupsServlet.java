f1e5ad574f4a27178730766c73ad3ccf92be4f67
Valeria Lopez <val.lopez1011@gmail.com>
2020-07-06 17:00:49 +0000

    Uses ImmutableMap instead of HashMap, and sets up template to iterate through multiple groups (from Datastore).

--- src/main/java/com/google/sps/servlets/GroupsServlet.java
+++ src/main/java/com/google/sps/servlets/GroupsServlet.java
@@ -2,10 +2,11 @@ package com.google.sps.servlets;
 
 import com.google.template.soy.SoyFileSet;
 import com.google.template.soy.tofu.SoyTofu;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableList.Builder;
+import com.google.common.collect.ImmutableMap;
 import java.io.File;
 import java.io.IOException;
-import java.util.Map;
-import java.util.HashMap;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -20,14 +21,19 @@ public class GroupsServlet extends HttpServlet {
      */
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
-        Map<String, String> data = new HashMap<>();
+        ImmutableList.Builder<ImmutableMap<String, String>> builder = new ImmutableList.Builder<>();
 
         // Hard coded data for now but will use data from datastore
         // as groups are added to datastore
-        data.put("groupImage", "https://images.unsplash.com/photo-1591622414912-34f2a8f8172e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80");
-        data.put("groupName", "Black Lives Matter");
-        data.put("groupDescription", "Group description would go here...");
+        ImmutableMap<String, String> groupMap = ImmutableMap.of("groupName", "Black Lives Matter",
+            "groupImage", "https://images.unsplash.com/photo-1591622414912-34f2a8f8172e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+            "groupDescription", "Group description would go here...");
+        builder.add(groupMap);
+        ImmutableList<ImmutableMap<String, String>> groups = builder.build();
+        
+        // Each group has its own map which points to its info and all maps are passed into the template as a list
+        // This will make it easier when groups are queried from Datastore
+        ImmutableMap<String, ImmutableList<ImmutableMap<String, String>>> data = ImmutableMap.of("groups", groups);
 
         SoyFileSet sfs = SoyFileSet
             .builder()
