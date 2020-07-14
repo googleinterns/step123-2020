6afc0493b4edb1096642683bf894f71b9f2dcf56
ahlschlager <ahlschlager@wisc.edu>
2020-07-14 23:25:43 +0000

    Add marker map (algo complex), checkbox functionality, test (issues atm)

--- src/main/webapp/mapScript.js
+++ src/main/webapp/mapScript.js
@@ -5,30 +5,35 @@ goog.require('templates.infoWindow');
 // Array that will hold the groups and there markers in a 2D Array
 // The array will allow the markers to be easilly all disabled as 
 // Groups were unchecked or checked 
-let groupArray = []; 
+let groupMarkers = new Map(); 
+
+// Default map placement centered at Mountain View, CA
+const DEFAULT_LAT = 37.3868;
+const DEFAULT_LNG = -122.085;
+const DEFAULT_ZOOM = 10;
+ 
+let map; 
 
 /**
  * Initialize the map to show Mountain View and events in that area
  */
 function initMap() {
-  
-    // Coordinates for the default map center of Mountain View, CA
-    const mapViewDefault = {lat: 37.3868, lng: -122.085}; 
+    const mapViewDefault = {lat: DEFAULT_LAT, lng: DEFAULT_LNG}; 
 
-    const map = new google.maps.Map(goog.dom.getElement('map'), {
-    // Zoom set to 8 as default every search will auto zoom to location
-        zoom: 8,
+    map = new google.maps.Map(goog.dom.getElement('map'), {
+        // Zoom level will change automatically with user's search
+        zoom: DEFAULT_ZOOM,
         center: mapViewDefault,
     });
 
     const geocoder = new google.maps.Geocoder();
     // TODO: repace the hardcoded event ID with actually value
-    getMarkerInfo(123, geocoder, map);
+    getMarkerInfo('123', geocoder);
 
-    autoCompleteAndZoom(map); 
+    autoCompleteAndZoom(); 
 
     document.getElementById('submit').addEventListener('click', () => {
-        geocodeAddress(geocoder, map);
+        geocodeAddress(geocoder);
     });
 
 }
@@ -38,7 +43,7 @@ function initMap() {
  * Allows user to hit enter instead of button
  * Automatically zooms according to the location inputed
  */
-function autoCompleteAndZoom(map) {
+function autoCompleteAndZoom() {
     const address = document.getElementById('address');
     let searchBox = new google.maps.places.SearchBox(address);
 
@@ -75,12 +80,12 @@ function autoCompleteAndZoom(map) {
 /**
  * Finds the location on the map for the given address
  */
-function geocodeAddress(geocoder, resultsMap) {
+function geocodeAddress(geocoder) {
     const address = document.getElementById('address').value;
 
     geocoder.geocode({'address': address}, (results, status) => {
         if (status === 'OK') {
-            resultsMap.setCenter(results[0].geometry.location);
+            map.setCenter(results[0].geometry.location);
         } else {
             alert('Geocode was not successful for the following reason: ' + status);
         }
@@ -95,10 +100,8 @@ function geocodeAddress(geocoder, resultsMap) {
 function toggleGroupMarkers(checkbox, groupId) {
     // Get the group name from the checkbox that was checked 
     if (checkbox.checked) {
-        window.alert('Checked group:' + groupId);
         addGroupMarkers(groupId);
     } else {
-        window.alert('Unchecked group:' + groupId);
         removeGroupMarkers(groupId);
     }
 }
@@ -107,23 +110,24 @@ function toggleGroupMarkers(checkbox, groupId) {
  * Adds a group and its events to the map based on the group passed
  */
 function addGroupMarkers(groupId) {
-    // Get event's with GroupID 
-    // For each event in the group 
-    // Call getMarkerInfo on eventId
+    if(groupMarkers.has(groupId)) {
+        setMapMarkers(map, groupId);
+    }
 }
 
 /**
  * Removes the markers of the given group from the map
  */
 function removeGroupMarkers(groupId) { 
-    // For each event in the group,
-    // hide each of the markers on map
+    if(groupMarkers.has(groupId)) {
+        setMapMarkers(null, groupId);
+    }
 }
 
 /**
  * Fetch all the events to add to the map 
  */
-function getMarkerInfo(eventId, geocoder, map) {
+function getMarkerInfo(groupId, geocoder) {
     // Fetch the json with the eventMarker objects 
     // For each object call addMarker()
     const address = 'San Diego, CA'; 
@@ -133,7 +137,7 @@ function getMarkerInfo(eventId, geocoder, map) {
     geocoder.geocode({'address': address}, (results) => {
         // Geocoded the address to longitude and lattitude for
         // Marker placement
-        addMarker(results[0].geometry.location, description, name, map);
+        addMarkerToMap(results[0].geometry.location, description, name, groupId);
     });
 }
 
@@ -142,7 +146,7 @@ function getMarkerInfo(eventId, geocoder, map) {
  * Using the eventID get the name and description for
  * the infoWindow 
  */
-function addMarker(address, descriptionText, nameText, map) {
+function addMarkerToMap(address, descriptionText, nameText, groupId) {
     const infoContent = templates.infoWindow.getMarkerInfo({
         'name': nameText, 
         'description': descriptionText,
@@ -159,4 +163,27 @@ function addMarker(address, descriptionText, nameText, map) {
     });
       
     marker.addListener('click', () => infoWindow.open(map,marker));
+    addToGroupMarkers(groupId, marker);
 }
+
+/**
+ * Add marker to the list with the groupId as the key
+ * Create add the key/value pair if not already there
+ */
+function addToGroupMarkers(groupId, marker) {
+    if (!(groupMarkers.has(groupId))) {
+        groupMarkers.set(groupId, new Array(marker));
+    } else {
+        groupMarkers.get(groupId).add(marker); 
+    }
+}
+
+// Sets the map on all markers in the array.
+function setMapMarkers(map, groupId) {
+    const markers = groupMarkers.get(groupId);
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+    }
+}
+
+module.exports = addToGroupMarkers;
