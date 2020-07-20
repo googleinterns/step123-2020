94d9fed41439589a87114e8a5e07c0cffdb99bb1
Dylan Castillo <dylanjcast@gmail.com>
2020-07-20 15:58:31 +0000

    Hardcode creation of groups

--- src/main/java/com/google/sps/data/Group.java
+++ src/main/java/com/google/sps/data/Group.java
@@ -2,11 +2,15 @@ package com.google.sps.data;
 
 public final class Group {
   private final String name;
+  private final String image;
+  private final String description;
   private final long groupId;
   private final String calendarId;
 
-  public Group(String name, long groupId, String calendarId) { 
+  public Group(String name, String image, String description, long groupId, String calendarId) { 
     this.name = name;
+    this.image = image;
+    this.description = description;
     this.groupId = groupId;
     this.calendarId = calendarId;
   }
--- src/main/java/com/google/sps/servlets/GroupsServlet.java
+++ src/main/java/com/google/sps/servlets/GroupsServlet.java
@@ -1,10 +1,15 @@
 package com.google.sps.servlets;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
 import com.google.template.soy.SoyFileSet;
 import com.google.template.soy.tofu.SoyTofu;
+import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableList.Builder;
 import com.google.common.collect.ImmutableMap;
+import com.google.sps.utils.ServletUtils;
 import java.io.File;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
@@ -12,35 +17,76 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * Servlet for displaying and creating groups.
+ */
 @WebServlet("/groups")
 public class GroupsServlet extends HttpServlet {
-    
-    /**
-     * Renders a HTML template with cards representing the various
-     * groups that have already been created.
-     */
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-        // Hard coded data for now but will use data from datastore
-        // as groups are added to datastore
-        // Photo by Hybrid on Unsplash (https://unsplash.com/@artbyhybrid?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText)
-        ImmutableMap<String, String> groupMap = ImmutableMap.of("groupName", "Black Lives Matter",
-            "groupImage", "https://images.unsplash.com/photo-1591622414912-34f2a8f8172e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-            "groupDescription", "Group description would go here...");
-        ImmutableList<ImmutableMap<String, String>> groups = ImmutableList.of(groupMap);
-        
-        // Each group has its own map which points to its info and all maps are passed into the template as a list
-        // This will make it easier when groups are queried from Datastore
-        ImmutableMap<String, ImmutableList<ImmutableMap<String, String>>> data = ImmutableMap.of("groups", groups);
+  /**
+   * Renders a HTML template with cards representing the various
+   * groups that have already been created.
+   */
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // Hard coded data for now but will use data from datastore
+    // as groups are added to datastore
+    // Photo by Hybrid on Unsplash (https://unsplash.com/@artbyhybrid?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText)
+    ImmutableMap<String, String> groupMap = ImmutableMap.of("groupName", "Black Lives Matter",
+        "groupImage", "https://images.unsplash.com/photo-1591622414912-34f2a8f8172e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+        "groupDescription", "Group description would go here...");
+    ImmutableList<ImmutableMap<String, String>> groups = ImmutableList.of(groupMap);
+      
+    // Each group has its own map which points to its info and all maps are passed into the template as a list
+    // This will make it easier when groups are queried from Datastore
+    ImmutableMap<String, ImmutableList<ImmutableMap<String, String>>> data = ImmutableMap.of("groups", groups);
 
-        SoyFileSet sfs = SoyFileSet
-            .builder()
-            .add(new File("../../src/main/java/templates/groups.soy"))
-            .build();
-        SoyTofu tofu = sfs.compileToTofu();
+    SoyFileSet sfs = SoyFileSet
+        .builder()
+        .add(new File("../../src/main/java/com/google/sps/templates/groups.soy"))
+        .build();
+    SoyTofu tofu = sfs.compileToTofu();
 
-        String out = tofu.newRenderer("templates.groups.groupsPage").setData(data).render();
-        response.getWriter().println(out);
+    String out = tofu.newRenderer("templates.groups.groupsPage").setData(data).render();
+    response.getWriter().println(out);
+  }
+
+  /**
+   * Create a group. Parameters are name, description (optional), and image.
+   */
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    //TODO: Unhardcode
+    String name = "Black Lives Matter";
+    String image = "https://images.unsplash.com/photo-1591622414912-34f2a8f8172e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80";
+
+    if(!Strings.isNullOrEmpty(name) && !Strings.isNullOrEmpty(image)) {
+      //TODO: Unhardcode
+      String groupId = createGroup(name, image, "Group description would go here");
+
+      response.setContentType("text/html");
+      response.getWriter().println(groupId);
     }
+  }
+
+  /**
+   * Returns the Group ID as a string.
+   */
+  private String createGroup(String name, String image, String description){
+    long groupId = 123L;
+    Entity groupEntity = new Entity(ServletUtils.GROUP_CONSTANT, String.valueOf(groupId));
+
+    groupEntity.setProperty("name", name);
+    groupEntity.setProperty("image", image);
+    groupEntity.setProperty("description", description);
+    //TODO: Remove once not hardcoded, since the CalendarServlet deals with setting the calendar ID.
+    groupEntity.setProperty("calendarId", "fk6u4m5isbl8i6cj1io1pkpli4@group.calendar.google.com");
+    //TODO: Unhardcode
+    groupEntity.setProperty("groupId", groupId);
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore.put(groupEntity);
+
+    return String.valueOf(groupId);
+  }
 }
