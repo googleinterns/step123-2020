40a39710a609b40149379ed826bb359b987eae3d
Dylan Castillo <dylanjcast@gmail.com>
2020-07-28 18:28:13 +0000

    Fix more merge issues

--- src/main/webapp/calendar.js
+++ src/main/webapp/calendar.js
@@ -1,3 +1,13 @@
+goog.require('goog.dom');
+goog.require('goog.dom.TagName');
+goog.require('templates.eventsoy');
+
 function initEvents() {
+  addressAutoComplete();
   //TODO: Events POST form
+}
+
+function addressAutoComplete() {
+  const address = document.getElementById('address');
+  const searchBox = new google.maps.places.SearchBox(address);
 }
