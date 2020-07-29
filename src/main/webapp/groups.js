3140481fd6241285b44bf81457ebab9e4b90b2a7
Valeria Lopez <val.lopez1011@gmail.com>
2020-07-29 19:14:39 +0000

    Adds form for creating new groups

---
+++ src/main/webapp/groups.js
@@ -0,0 +1,13 @@
+var newGroupContainer;
+
+function init() {
+    newGroupContainer = document.getElementById("new-group-container");
+}
+
+function showForm() {
+    newGroupContainer.style.display = "flex";
+}
+
+function hideForm() {
+    newGroupContainer.style.display = "none";
+}
