4562ad089d07c4500b16e62811cdf7d5bac3243d
ahlschlager <ahlschlager@wisc.edu>
2020-07-31 23:12:54 +0000

    Move groups list to Servlet Utils

--- src/main/java/com/google/sps/servlets/MapServlet.java
+++ src/main/java/com/google/sps/servlets/MapServlet.java
@@ -1,16 +1,9 @@
 package com.google.sps.servlets;
- 
+
+import static com.google.sps.utils.ServletUtils.getGroupsList;
 import static com.google.sps.utils.SoyRendererUtils.getOutputString;
 import static com.google.sps.utils.StringConstants.*;
- 
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.EntityNotFoundException;
-import com.google.appengine.api.datastore.KeyFactory;
-import com.google.appengine.api.datastore.PreparedQuery;
-import com.google.appengine.api.datastore.Query;
-import com.google.appengine.api.datastore.Query.FilterOperator;
+
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.sps.utils.ServletUtils;
@@ -42,43 +35,8 @@ public class MapServlet extends HttpServlet {
         final String apiKey = scanner.nextLine();
         scanner.close();
 
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-        String userEmail = request.getUserPrincipal().getName();
-        // Stores the IDs for all the groups the user ahs joined
-        HashSet<Long> userGroups = new HashSet<Long>();
+        ImmutableList<ImmutableMap<String, String>> groupsList = getGroupsList(request); 
 
-        Query userQuery = new Query(USER_KIND).addFilter(Entity.KEY_RESERVED_PROPERTY, FilterOperator.EQUAL, userEmail);
-        PreparedQuery userPreparedQuery = datastore.prepare(userQuery);
-        Entity user = userPreparedQuery.asSingleEntity();
-
-        if (user != null) {
-            userGroups = (HashSet<Long>) user.getProperty(GROUPS_KEY);
-
-        } else {
-            // if the user does not exist, create one and add to datastore
-            user = new Entity(USER_KIND, userEmail);
-
-            user.setProperty(USER_EMAIL_PROPERTY, userEmail);
-            user.setProperty(GROUPS_KEY, userGroups);
-
-            datastore.put(user);
-        }
-
-        ImmutableList.Builder<ImmutableMap<String, String>> groupsListBuilder = new ImmutableList.Builder<>();
-        for (Long groupId : userGroups) {
-            try {
-                Entity group = datastore.get(KeyFactory.createKey(GROUP_KIND, groupId));
-                ImmutableMap<String, String> groupMap = ImmutableMap.of(
-                    GROUP_NAME_PROPERTY, (String) group.getProperty(GROUP_NAME_PROPERTY),
-                    GROUP_ID_PROPERTY, group.getKey().getName());
-                groupsListBuilder.add(groupMap);
-            } catch (EntityNotFoundException invalidGroup) {
-                continue;
-            }
-            
-        }
-        ImmutableList<ImmutableMap<String, String>> groupsList = groupsListBuilder.build();
- 
         final String mapPageHtml = getOutputString(MAP_SOY_FILE, MAP_TEMPLATE_NAMESPACE, 
             ImmutableMap.of(GROUPS_KEY, groupsList, API_KEY_NAME, apiKey));
  
--- src/main/java/com/google/sps/utils/ServletUtils.java
+++ src/main/java/com/google/sps/utils/ServletUtils.java
@@ -8,10 +8,16 @@ import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.EntityNotFoundException;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.base.Strings;
 import com.google.gson.Gson;
 import java.io.IOException;
 import java.lang.ClassLoader;
+import java.util.HashSet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -63,4 +69,49 @@ public final class ServletUtils {
       return;
     }
   }
+
+  public static ImmutableList<ImmutableMap<String, String>> getGroupsList(HttpServletRequest request) {
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    String userEmail = request.getUserPrincipal().getName();
+    // Stores the IDs for all the groups the user ahs joined
+    HashSet<Long> userGroups = new HashSet<Long>();
+
+    Query userQuery = new Query(USER_KIND).addFilter(Entity.KEY_RESERVED_PROPERTY, FilterOperator.EQUAL, userEmail);
+    PreparedQuery userPreparedQuery = datastore.prepare(userQuery);
+    Entity user = userPreparedQuery.asSingleEntity();
+
+    if (user != null) {
+      userGroups = (HashSet<Long>) user.getProperty(GROUPS_KEY);
+
+    } else {
+      // if the user does not exist, create one and add to datastore
+      user = new Entity(USER_KIND, userEmail);
+
+      user.setProperty(USER_EMAIL_PROPERTY, userEmail);
+      user.setProperty(GROUPS_KEY, userGroups);
+
+      datastore.put(user);
+    }
+
+    ImmutableList.Builder<ImmutableMap<String, String>> groupsListBuilder = new ImmutableList.Builder<>();
+    for (Long groupId : userGroups) {
+      try {
+        Entity group = datastore.get(KeyFactory.createKey(GROUP_KIND, groupId));
+        ImmutableMap<String, String> groupMap = ImmutableMap.of(
+            GROUP_NAME_PROPERTY, (String) group.getProperty(GROUP_NAME_PROPERTY),
+            GROUP_ID_PROPERTY, group.getKey().getName(),
+            GROUP_DESCRIPTION_PROPERTY, (String) group.getProperty(GROUP_DESCRIPTION_PROPERTY),
+            GROUP_CALENDARID_PROPERTY, (String) group.getProperty(GROUP_CALENDARID_PROPERTY),
+            GROUP_IMAGE_PROPERTY, (String) group.getProperty(GROUP_IMAGE_PROPERTY));
+        groupsListBuilder.add(groupMap);
+      } catch (EntityNotFoundException invalidGroup) {
+        continue;
+      }
+            
+    }
+    ImmutableList<ImmutableMap<String, String>> groupsList = groupsListBuilder.build();
+    return groupsList; 
+  }
+
+
 }
